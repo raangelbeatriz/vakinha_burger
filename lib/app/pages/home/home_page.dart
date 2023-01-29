c8fd33905aeb04bde96376275e315996f3d74ec1
Beatriz Rangel <raangelbeatriz@gmail.com>
2023-01-29 17:57:53 -0300

    add helpers

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burguer/app/core/ui/helpers/messagea.dart';
 import 'package:vakinha_burguer/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:vakinha_burguer/app/models/product_model.dart';
 import 'package:vakinha_burguer/app/pages/home/widgets/delivery_product_tile.dart';
 
-class HomePage extends StatelessWidget {
+import '../../core/ui/helpers/loader.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,20 +22,19 @@ class HomePage extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: ((context, index) {
-                  return DeliveryProductTile(
-                    product: ProductModel(
-                        id: 1,
-                        name: 'X-Tudo',
-                        description:
-                            'Lanche acompanha pão, hambúguer, mussarela, alface, tomate e maionese',
-                        img:
-                            'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
-                        price: 15.00),
-                  );
-                }),
-              ),
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return DeliveryProductTile(
+                      product: ProductModel(
+                          id: 1,
+                          name: 'X-Tudo',
+                          description:
+                              'Lanche acompanha pão, hambúguer, mussarela, alface, tomate e maionese',
+                          img:
+                              'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
+                          price: 15.00),
+                    );
+                  }),
             ),
           ],
         ));
